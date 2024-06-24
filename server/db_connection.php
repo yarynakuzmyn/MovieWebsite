c5577c2d8e733b122180b49988de00ca6effe1e2
yarynakuzmyn <yaryna.kuzmyn@kpk-lp.com.ua>
2024-06-25 00:43:34 +0300

    Added DB_Connection file

---
+++ server/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "movie_website";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Database connection failed: " . $e->getMessage();
+}
+?>
